5e7a18c61ec7dc5e13b6f8532b15ba769ec6624d
ap080221063 <ap080221063@gmail.com>
2016-12-27 21:00:40 +0000

    first typescript experiment! awesome!

---
+++ app/magic.js
@@ -0,0 +1,8 @@
+var message = 'hello';
+function saySomething(message) {
+    if (message === void 0) { message = "fuck you, no value inputed!"; }
+    console.log(message);
+}
+saySomething('fuck!');
+//saySomething(55); 
+//# sourceMappingURL=magic.js.map
---
+++ app/magic.ts
@@ -0,0 +1,8 @@
+const message = 'hello';
+
+function saySomething(message: string = "fuck you, no value inputed!"){
+console.log(message);
+}
+
+saySomething('fuck!');
+//saySomething(55);
