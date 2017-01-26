afaec36eb470e08e46a99401ec895f2605ab26a9
ap080221063 <ap080221063@gmail.com>
2017-01-26 23:27:56 +0000

    first conditional and iterating elements in angular 2

--- app/app.component.js
+++ app/app.component.js
@@ -8,13 +8,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello!';
+        this.users = [
+            { id: 1, name: 'Andre', username: 'ap080221063' },
+            { id: 2, name: 'Bruno', username: 'br080321123' },
+            { id: 3, name: 'Hugo', username: 'hg080244455' }
+        ];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: "my-app",
-        template: "\n       <div class=\"jumbotron\">\n         <h1>Welcome to our APP!</h1>\n       </div>\n    ",
+        template: "\n<header>\n    <nav class=\"navbar navbar-inverse\">\n        <div class=\"navbar-header\">\n            <a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n        </div>\n    </nav>\n<header>\n\n<main>\n    <div class=\"jumbotron\">\n        <h1>Welcome to our APP!</h1>\n        <p>{{ message }}</p>\n    </div>\n\n    <!--<p>The user is {{ user.name }} ({{user.username}}).</p>-->\n\n    <div *ngIf=\"users\">\n        <div  *ngFor=\"let user of users\">\n            {{ user.name }} - ({{user.username}})\n        </div>\n    </div>\n\n</main>\n\n<footer class=\"text-center\">\n    Copyright &copy; 2017\n</footer>\n    ",
         styles: ["\n      .jumbotron {box-shadow: 0 2px 0 rgba(0,0,0,0.2)}\n    "]
     })
 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,33 @@ import { Component } from '@angular/core';
 @Component({
     selector:"my-app",
     template:`
-       <div class="jumbotron">
-         <h1>Welcome to our APP!</h1>
-       </div>
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+            <a href="/" class="navbar-brand">My Angular 2 App!</a>
+        </div>
+    </nav>
+<header>
+
+<main>
+    <div class="jumbotron">
+        <h1>Welcome to our APP!</h1>
+        <p>{{ message }}</p>
+    </div>
+
+    <!--<p>The user is {{ user.name }} ({{user.username}}).</p>-->
+
+    <div *ngIf="users">
+        <div  *ngFor="let user of users">
+            {{ user.name }} - ({{user.username}})
+        </div>
+    </div>
+
+</main>
+
+<footer class="text-center">
+    Copyright &copy; 2017
+</footer>
     `,
     styles:[`
       .jumbotron {box-shadow: 0 2px 0 rgba(0,0,0,0.2)}
@@ -13,4 +37,11 @@ import { Component } from '@angular/core';
 
 })
 
-export class AppComponent{}
+export class AppComponent{
+    message = 'Hello!';
+    users = [
+             { id: 1, name: 'Andre', username: 'ap080221063'},
+             { id: 2, name: 'Bruno', username: 'br080321123'},
+             { id: 3, name: 'Hugo', username: 'hg080244455'}
+            ]
+}
